0baaa37ba7a5a9cf3abd80e3bd90b78e252445ba
Hai-VanNgo <ngovanhai120799@gmail.com>
2024-10-27 14:59:02 +0700

    update decorator

--- decorator.py
+++ decorator.py
@@ -1,6 +1,6 @@
 import functools
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import jwt
 from flask import request, abort
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 JWT_SECRET = os.environ['JWT_SECRET']
+JWT_ALGORITHM = 'HS256'
 
 def require_jwt(f):
     """
@@ -20,13 +21,13 @@ def require_jwt(f):
     return decorated_function
 
 def encoded_jwt(body):
-    exp_time = datetime.now() + timedelta(days=1)
+    exp_time = datetime.now(tz=timezone.utc) + timedelta(hours=1)
     payload = {
         'exp': exp_time,
-        'nbf': datetime.now(),
+        'nbf': datetime.now(tz=timezone.utc),
         'email': body.get('email')
     }
-    return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+    return jwt.encode(payload, JWT_SECRET)
 
 def decode_jwt():
     """
@@ -35,8 +36,9 @@ def decode_jwt():
     if not 'Authorization' in request.headers:
         abort(401)
     data = request.headers['Authorization']
-    token = str.replace(str(data), 'Bearer', '')
+    token = str.replace(str(data), 'Bearer ','')
     try:
-        return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except: # pylint: disable=bare-except
+        response = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return response
+    except Exception as e: # pylint: disable=bare-except
         abort(401)
